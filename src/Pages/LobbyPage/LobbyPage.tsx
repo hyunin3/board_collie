befa03bf0a6f50ce5014c48e82cf21d158218f65
stock4share2@gmail.com <stock4share2@gmail.com>
2023-10-23 10:11:35 +0900

    로비 페이지 수정

--- src/Pages/LobbyPage/LobbyPage.tsx
+++ src/Pages/LobbyPage/LobbyPage.tsx
@@ -19,34 +19,71 @@ const LobbyPage: React.FC = () => {
 
   return (
     <div className="container">
-      <img src={logo} alt="App Logo" className="logo" />
-      <Typography variant="h5" gutterBottom style={{ fontFamily: 'Jua, sans-serif' }}>
-        게임을 즐기실 인원을 선택해주세요!
-      </Typography>
-      <div className="player-adjust">
-        <IconButton onClick={decrementPlayers} disabled={players === 1}>
-          <RemoveIcon />
-        </IconButton>
-        <Typography variant="h6">{players}</Typography>
-        <IconButton onClick={incrementPlayers} disabled={players === 8}>
-          <AddIcon />
-        </IconButton>
-      </div>
-      <Link to="/main">
-      <Button 
-        variant="contained" 
+      <img src={logo} alt="App Logo" className="logo" style={{ marginBottom: '40px' }} />
+      <Typography 
+        variant="h3" 
+        gutterBottom 
         style={{ 
-          backgroundColor: '#CCF38C', 
-          color: 'black', 
-          marginTop: '20px', 
-          fontFamily: 'Jolly Lodger, cursive',
-          borderRadius: "10px",
-          fontSize: "2rem", 
-          padding: '1px 70px',
+          fontFamily: 'Jua, sans-serif', 
+          whiteSpace: 'pre-wrap', 
+          marginBottom: '40px' 
         }}
       >
-        START
-      </Button>
+        {Array.from('게임을 즐기실 인원을 선택해주세요!').map((char, index) => (
+          <span key={index} style={{ display: 'inline-block', '--i': index } as React.CSSProperties}>
+            {char}
+          </span>
+        ))}
+      </Typography>
+
+      <div className="player-adjust" style={{ marginBottom: '40px' }}>
+        <IconButton 
+          onClick={decrementPlayers} 
+          disabled={players === 1} 
+          style={{ backgroundColor: 'black', color: 'white', width: '50px', height: '50px', marginRight: '20px' }}
+        >
+          <RemoveIcon style={{ fontSize: '30px' }} />
+        </IconButton>
+        
+        <Typography 
+          variant="h6" 
+          style={{
+            width: '50px', 
+            textAlign: 'center', 
+            lineHeight: '50px', 
+            fontSize: '30px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {players}
+        </Typography>
+
+        <IconButton 
+          onClick={incrementPlayers} 
+          disabled={players === 8} 
+          style={{ backgroundColor: 'black', color: 'white', width: '50px', height: '50px', marginLeft: '20px' }}
+        >
+          <AddIcon style={{ fontSize: '30px' }} />
+        </IconButton>
+      </div>
+
+      <Link to="/main">
+        <Button 
+          variant="contained" 
+          style={{ 
+            backgroundColor: '#CCF38C', 
+            color: 'black', 
+            marginTop: '20px', 
+            fontFamily: 'Jolly Lodger, cursive',
+            borderRadius: "10px",
+            fontSize: "2rem", 
+            padding: '1px 70px',
+          }}
+        >
+          START
+        </Button>
       </Link>
     </div>
   );
