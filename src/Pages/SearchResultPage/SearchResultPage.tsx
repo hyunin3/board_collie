5a766d7d86959779cbb8474093395275f20a7027
stock4share2@gmail.com <stock4share2@gmail.com>
2023-10-24 13:30:34 +0900

    칩으로 태그 표시

--- src/Pages/SearchResultPage/SearchResultPage.tsx
+++ src/Pages/SearchResultPage/SearchResultPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
-import { Divider, Grid } from '@mui/material';
+import { Divider, Grid, Chip, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './SearchResultPage.css';
 import RecentGamesList from '../../components/RecentGamesList/RecentGamesList';
@@ -113,49 +113,47 @@ const SearchResultsPage: React.FC = () => {
 
   return (
     <div>
-      <SearchBar onSearch={handleSearch} />
-      <FilterBar numberOfPlayers={numberOfPlayers} setNumberOfPlayers={setNumberOfPlayers} />
-        <Grid container spacing={2}>
-          <Grid item xs={9}>
-   
-      {results.length === 0 ? (
-      <div style={centerStyle}>검색 결과가 없습니다.</div>
-    ) : (
-      <div>
-        {results.map((item, index) => (
-          <div key={item.name}>
-            <h3>
-              <Link 
-                to={`/game/${item.name}`} 
-                style={{ textDecoration: 'none', color: 'inherit' }}
-                onClick={() => handleGameClick(item.name)}
-                >
-                {item.name}
-              </Link>
-            </h3>
-            <ul>
-              {item.tags.map(tag => (
-                <li key={tag}>{tag}</li>
-              ))}
-            </ul>
-            {index < results.length - 1 && <Divider />}
+    <SearchBar onSearch={handleSearch} />
+    <FilterBar numberOfPlayers={numberOfPlayers} setNumberOfPlayers={setNumberOfPlayers} />
+    <Grid container spacing={2}>
+      <Grid item xs={9}>
+        {results.length === 0 ? (
+          <div style={centerStyle}>검색 결과가 없습니다.</div>
+        ) : (
+          <div>
+            {results.map((item, index) => (
+              <div key={item.name}>
+                <h3>
+                  <Link 
+                    to={`/game/${item.name}`} 
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                    onClick={() => handleGameClick(item.name)}
+                  >
+                    {item.name}
+                  </Link>
+                </h3>
+                <Stack direction="row" spacing={1} style={{ marginBottom: '20px' }}>
+                  {item.tags.map(tag => (
+                    <Chip key={tag} label={tag} />
+                  ))}
+                </Stack>
+                {index < results.length - 1 && <Divider />}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    )}
-  </Grid>
-  <Grid item container xs={3} style={{ alignItems: 'flex-start' }}>
-    <Divider orientation="vertical" flexItem sx={{  height: '100%' }} />
-    <Grid item xs style={{ overflowY: 'auto' }} className="hide-scrollbar">
-      <div style={{ textAlign: 'center' }}>
-        <h4>최근 본 게임</h4>
-        <RecentGamesList /> 
-      </div>
+        )}
+      </Grid>
+      <Grid item container xs={3} style={{ alignItems: 'flex-start' }}>
+        <Divider orientation="vertical" flexItem sx={{ height: '100%' }} />
+        <Grid item xs style={{ overflowY: 'auto' }} className="hide-scrollbar">
+          <div style={{ textAlign: 'center' }}>
+            <h4>최근 본 게임</h4>
+            <RecentGamesList />
+          </div>
+        </Grid>
+      </Grid>
     </Grid>
-  </Grid>
-</Grid>
-
-    </div>
+  </div>
   );
 }
 
